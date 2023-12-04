cdc1f9c3fcce1e8db19a995b215866d8fdb3b7d9
danielb <dan.dam@hotmail.com>
2023-12-04 07:56:59 -0400

    primeiro commit

---
+++ personagem.java
@@ -0,0 +1,33 @@
+﻿public class personagem {
+    String nome;
+    int idade;
+    String tipo;
+    String atack;
+    
+    public void atacar(String tipo){
+    this.tipo = tipo;
+        
+    switch (tipo) {
+        case "Guerreiro":
+            atack = "espada";
+            System.out.println("O herói chamado de " + nome + " é da classe " + tipo + ", de " + idade + " anos, atacou com " + atack);
+            break;
+        case "Mago":
+            atack = "magia";
+            System.out.println("O herói chamado de " + nome + " é da classe " + tipo + ", de " + idade + " anos, atacou com " + atack);
+            break;
+        case "Monge":
+            atack = "artes marciais";
+            System.out.println("O herói chamado de " + nome + " é da classe " + tipo + ", de " + idade + " anos, atacou com " + atack);
+            break;
+        case "Ninja":
+            atack = "Shuriken";
+            System.out.println("O herói chamado de " + nome + " é da classe " + tipo + ", de " + idade + " anos, atacou com " + atack);
+            break;
+        default:
+            break;
+            
+    }
+}
+
+}
---
+++ personagemSelecionavel.java
@@ -0,0 +1,26 @@
+﻿import java.util.Scanner;
+
+public class personagemSelecionavel {
+    
+    public static void main(String[] args) {
+        personagem heroi = new personagem();
+        heroi.nome = "Daniel";
+        heroi.idade = 24;
+        heroi.tipo = "Guerreiro";
+        heroi.atack = "espada";
+
+        heroi.atacar(heroi.tipo);
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Herói personalizado!\nDigite o nome do seu herói: ");
+        heroi.nome = teclado.nextLine();
+        System.out.print("Digite a idade do seu herói: ");
+        heroi.idade = teclado.nextInt();
+        teclado.nextLine();
+        System.out.print("Digite a classe do seu herói: ");
+        heroi.tipo = teclado.nextLine();
+        heroi.atacar(heroi.tipo);
+    }
+    
+}
+
